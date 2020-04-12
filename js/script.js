5c95a9d63b6ae615dedbe6b53c86c5ddcbf63555
Jasmine Chan <chan.jasmine4@gmail.com>
2020-04-12 16:54:51 +0800

    sphere

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // TODO Webpack
 
 import * as THREE from '../node_modules/three/build/three.module.js';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 // import * as THREE from '.build/three.module.js';
 // var THREE = require('../node_modules/three/build/three');
 
@@ -9,19 +10,37 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor("#eeeeee");
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
 
 camera.position.z = 5;
 
+// cube
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// sphere
+var sphereGeom = new THREE.SphereGeometry();
+var sphereMat = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+var sphere = new THREE.Mesh(sphereGeom, sphereMat);
+scene.add(sphere);
+
+var light = new THREE.PointLight(0xffffff, 1, 500);
+light.position.set(10, 0, 25);
+scene.add(light);
+
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += .01;
-    cube.rotation.y += .01;
+    // cube.rotation.x += .01;
+    // cube.rotation.y += .01;
     renderer.render(scene, camera);
 }
 
